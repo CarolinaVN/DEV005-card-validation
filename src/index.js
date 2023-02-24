d95a39e91174c80e88548f8fa5d9429056228bf7
Carolina Vera <cvera.nahuelcoy@gmail.com>
2023-02-24 14:50:45 -0300

    Se da el numero de tarjeta  a el archivo Validator y se revierte el sentido

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+
+//alert('Bienvenida al valrificador de tarjetas de crédito, utilizaremos el algoritmo de Lunh para realizar la validación');
+
+//const card = document.getElementById ('card');
+const cardNumber = document.getElementById('card-number');
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+//const validCard = document.getElementById('valid-card');
+//const invalidCard = document.getElementById('invalid-card');
+
+
+
+//Funciones que interactuan con el DOM
+function Valid() {
+  const enteros = []
+  //divide string en caracteres individuales
+  const cardCharacter = cardNumber.value.split('');
+  //console.log(cardCharacter);
+  // convierte los string a enteros 
+  for (let i = 0; i < cardCharacter.length; i++) {
+    const numero = parseInt(cardCharacter[i])
+    // el imput solo se recibe numeros
+    if (isNaN(numero)) {
+      result.innerHTML = 'Error al digitar el número de tarjeta';
+    } else {
+      enteros.push(numero)
+    }
+
+  }
+  console.log(validator.isValid(enteros));
+}
+
+//console.log(validator.isValid(123))
+
+
+//eventos del DOM
+btn.addEventListener('click', Valid);
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,14 @@
 const validator = {
-  // ...
+  isValid: function (credidCardNumber) {
+    let credidCardNumberReverse = credidCardNumber.reverse()
+    return credidCardNumberReverse
+  },
+
+
+  maskify: function (credidCardNumber) {
+    return 'String'
+  }
+
 };
 
 export default validator;
